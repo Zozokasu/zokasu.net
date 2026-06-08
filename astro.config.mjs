8914c9e96fce9f70c27f5f52ed70180abde7b8b9
ぞぞかす <jojohounds@gmail.com>
2026-06-08 19:36:09 +0900

    remove cloudflare settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
@@ -34,9 +32,5 @@ export default defineConfig({
               ],
           },
       },
-	],
-
-  adapter: cloudflare({
-    imageService: 'passthrough',
-  }),
+	]
 });
